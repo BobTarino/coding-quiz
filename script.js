94c54849a587ff024e034b749e5b5048e4546b6d
Bobby T <bob.tarino@gmail.com>
2021-04-08 21:14:48 -0500

    created start button and hide class

---
+++ script.js
@@ -0,0 +1,34 @@
+// var startScreen = document.getElementById("startScreen");
+// var gameScreen = document.getElementById("gameScreen");
+// var endScreen = document.getElementById("endScreen");
+// var startButton = document.getElementById("startButton");
+// var questionDisplay = document.getElementById("questionDisplay");
+// var answerDisplay = document.getElementById("answerDisplay");
+// var currentQuestion = 0;
+// var questionBank = [
+//     {
+//         question:"What is the third letter of the alphabet?",
+//         answers: ["A","F", "C", "Z"], 
+//         correct: "C"
+//     },
+//     {
+//         question:"What is the second letter of the alphabet?",
+//         answers: ["A","B", "L", "Z"], 
+//         correct: "B"
+//     },
+//     {
+//         question:"What is the last letter of the alphabet?",
+//         answers: ["A","F", "L", "Z"], 
+//         correct: "Z"
+//     }
+// ];
+
+// function displayQuestionsAnwers() {
+//     questionDisplay.textContent = questionBank[currentQuestion].question;
+// }
+// console.log(questionBank[currentQuestion].question)
+// startButton.addEventListener("click", function() {
+//     gameScreen.classList.remove("hide");
+//     startScreen.classList.add("hide");
+//     displayQuestionsAnwers();
+// });
