31febde6c38878c056622561d7fd8da0aa06903e
Bobby T <bob.tarino@gmail.com>
2021-04-08 22:04:29 -0500

    rrefactored code

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
-// var startScreen = document.getElementById("startScreen");
-// var gameScreen = document.getElementById("gameScreen");
-// var endScreen = document.getElementById("endScreen");
-// var startButton = document.getElementById("startButton");
-// var questionDisplay = document.getElementById("questionDisplay");
-// var answerDisplay = document.getElementById("answerDisplay");
-// var currentQuestion = 0;
-// var questionBank = [
-//     {
-//         question:"What is the third letter of the alphabet?",
-//         answers: ["A","F", "C", "Z"], 
-//         correct: "C"
-//     },
-//     {
-//         question:"What is the second letter of the alphabet?",
-//         answers: ["A","B", "L", "Z"], 
-//         correct: "B"
-//     },
-//     {
-//         question:"What is the last letter of the alphabet?",
-//         answers: ["A","F", "L", "Z"], 
-//         correct: "Z"
-//     }
-// ];
+var startScreen = document.getElementById("startScreen");
+var gameScreen = document.getElementById("gameScreen");
+var endScreen = document.getElementById("endScreen");
+var startButton = document.getElementById("startButton");
+var questionDisplay = document.getElementById("questionDisplay");
+var answerDisplay = document.getElementById("answerDisplay");
+var currentQuestion = 0;
+var questionBank = [
+    {
+        question:"Commonly used data types DO NOT include?",
+        answers: {
+            a: "strings",
+            b: "booleans",
+            c: "alerts",
+            d: "numbers",
+        },
+        correct: "d"
+    },
+    {
+        question:"Commonly used data types DO NOT include?",
+        answers: {
+            a: "strings",
+            b: "booleans",
+            c: "alerts",
+            d: "numbers",
+        },
+        correct: "d"
+    },
+    {
+        question:"Commonly used data types DO NOT include?",
+        answers: {
+            a: "strings",
+            b: "booleans",
+            c: "alerts",
+            d: "numbers",
+        },
+        correct: "d"
+    }
+];
 
-// function displayQuestionsAnwers() {
-//     questionDisplay.textContent = questionBank[currentQuestion].question;
-// }
-// console.log(questionBank[currentQuestion].question)
-// startButton.addEventListener("click", function() {
-//     gameScreen.classList.remove("hide");
-//     startScreen.classList.add("hide");
-//     displayQuestionsAnwers();
-// });
+function displayQuestionsAnwers() {
+    questionDisplay.textContent = questionBank[currentQuestion].question;
+}
+console.log(questionBank[currentQuestion].question)
+startButton.addEventListener("click", function() {
+    gameScreen.classList.remove("hide");
+    startScreen.classList.add("hide");
+    displayQuestionsAnwers();
+});
